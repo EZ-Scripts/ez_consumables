40cd487f21400ad8b30b54dfc57c4a49e1ca1be9
Rayaan Uddin <rayaan.uddin@outlook.com>
2025-01-19 06:34:53 +0000

    Updated fx and added readme

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,20 @@
 fx_version 'cerulean'
+lua54 'yes'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'ns_consumables'
+author 'Rayaan Uddin'
+description 'A RedM consumable system, allows you to make food/ drinks for all metabolism systems you use.'
 version '1.0'
 
 shared_scripts {
     'config.lua'
 }
 
-client_script 'client/main.lua'
-server_script 'server/main.lua'
+client_scripts {
+    'client/*.lua'
+}
 
-lua54 'yes'
+server_scripts {
+    'server/*.lua'
+}
